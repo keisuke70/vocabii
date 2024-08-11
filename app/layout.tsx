7fb83fb9cef09d628ce2350cdfc1b6739596f7d4
Ykeisuke <g1d4h@students.cs.ubc.ca>
2024-08-11 18:01:14 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.png" />
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
       </head>
       <body className={inter.className}>
         <div className="flex flex-col justify-between w-full min-h-screen">
