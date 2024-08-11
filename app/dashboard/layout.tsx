9cce740240d0cb39a1c3aaee0c84ef5b0ed0a4eb
Ykeisuke <g1d4h@students.cs.ubc.ca>
2024-08-11 11:26:31 +0900

    fix

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,16 +4,16 @@ export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col md:flex-row min-h-screen">
+    <div className="flex flex-col md:flex-row h-screen overflow-hidden">
       <div className="block md:hidden w-full h-auto">
         <DashboardHeader />
       </div>
-      
+
       <div className="hidden md:block w-full md:w-40 lg:w-64 flex-shrink-0 h-full">
         <SideNav />
       </div>
 
-      <div className="flex-grow p-3 md:p-10 overflow-y-auto">
+      <div className="flex-grow p-3 md:p-10 overflow-y-auto h-full">
         {children}
       </div>
     </div>
