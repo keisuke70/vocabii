3905b3ae3f3760676b8c7f4797a55b231fad0b03
Ykeisuke <g1d4h@students.cs.ubc.ca>
2024-08-16 18:00:51 +0900

    fix

--- app/ui/dashboard/wordbook/wordTable.tsx
+++ app/ui/dashboard/wordbook/wordTable.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { useState, useRef } from "react";
 import {
@@ -59,6 +59,7 @@ const WordTable: React.FC<WordTableProps> = ({ words }) => {
             <TableHead className="border-r col-span-2 md:col-span-1 border-gray-300">
               <div className="flex justify-center items-center h-full min-w-[40px] md:text-base text-xs whitespace-normal break-all">
                 Pron.
+                <FaCirclePlay className="ml-1 p-1 text-xl text-blue-600" />
               </div>
             </TableHead>
             <TableHead className="border-r col-span-2 md:col-span-1 border-gray-300">
@@ -81,17 +82,23 @@ const WordTable: React.FC<WordTableProps> = ({ words }) => {
                 onClick={() => handleWordClick(word.id)}
                 className="grid grid-cols-9 md:grid-cols-5 cursor-pointer hover:bg-gray-100 border-b border-gray-300 group"
               >
-                <TableCell className="text-xs col-span-2 md:col-span-1 md:text-base border-r font-medium border-gray-300 whitespace-normal break-all">
+                <TableCell className="p-2 text-xs col-span-2 md:col-span-1 md:text-base border-r font-medium border-gray-300 whitespace-normal break-all">
                   <div className="flex justify-center items-center h-full min-w-[20px]">
                     {word.word}
                   </div>
                 </TableCell>
-                <TableCell className="text-xs col-span-2 md:col-span-1 md:text-base border-r border-gray-300 whitespace-normal break-all">
-                  <div className="flex justify-center items-center h-full min-w-[26px]">
+                <TableCell className="p-2 text-xs col-span-2 md:col-span-1 md:text-base border-r border-gray-300 whitespace-normal break-all">
+                  <div
+                    className="flex justify-center items-center h-full min-w-[26px] cursor-pointer text-blue-600 hover:text-blue-900 rounded-lg bg-white"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      new Audio(word.audiourl).play();
+                    }}
+                  >
                     {word.pronunciation}
                   </div>
                 </TableCell>
-                <TableCell className="border-r col-span-2 md:col-span-1 border-gray-300 relative">
+                <TableCell className="p-1 border-r col-span-2 md:col-span-1 border-gray-300 relative">
                   <div className="flex justify-center items-center h-full min-w-[20px] whitespace-normal break-all">
                     <Button
                       className="rounded-full bg-gray-700 hover:bg-gray-800 md:text-base text-xs px-2 py-0.3"
