cb49faee066a06e9d116255be9a627cdd140b739
Ykeisuke <g1d4h@students.cs.ubc.ca>
2024-08-17 16:03:04 +0900

    fix

--- app/ui/dashboard/wordbook/wordTable.tsx
+++ app/ui/dashboard/wordbook/wordTable.tsx
@@ -9,10 +9,20 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectValue,
+} from "@/components/ui/select";
 import WordDetail from "./wordDetail";
-import { FaCirclePlay, FaChevronDown } from "react-icons/fa6";
+import { FaCirclePlay, FaChevronDown, FaStar } from "react-icons/fa6";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { updateWordPriority } from "@/lib/actions";
+
 import "./WordTable.css"; // Import the CSS file for transitions
 
 export interface word {
@@ -25,24 +35,49 @@ export interface word {
   detaileddescription: string;
   nounplural?: string | null;
   verbconjugations?: string | null;
+  priority?: string;
 }
 
 interface WordTableProps {
-  words: word[];
+  initialWords: word[]; // Rename the prop to `initialWords`
 }
 
-const WordTable: React.FC<WordTableProps> = ({ words }) => {
+const WordTable: React.FC<WordTableProps> = ({ initialWords }) => {
   const [selectedWordId, setSelectedWordId] = useState<number | null>(null);
   const nodeRef = useRef(null);
+  const [words, setWords] = useState<word[]>(initialWords);
 
   const handleWordClick = (wordId: number) => {
     if (selectedWordId === wordId) {
       setSelectedWordId(null);
     } else {
-      setSelectedWordId(null); // Close any open row first
+      setSelectedWordId(null);
       setTimeout(() => {
         setSelectedWordId(wordId);
-      }, 300); // Delay to allow closing animation to complete
+      }, 300);
+    }
+  };
+
+  const handlePriorityChange = async (wordId: number, newPriority: string) => {
+    setWords((prevWords) =>
+      prevWords.map((word) =>
+        word.id === wordId ? { ...word, priority: newPriority } : word
+      )
+    );
+    try {
+      await updateWordPriority(wordId, newPriority);
+    } catch (error) {
+      console.error("Failed to update priority:", error);
+      setWords((prevWords) =>
+        prevWords.map((word) =>
+          word.id === wordId
+            ? {
+                ...word,
+                priority: prevWords.find((w) => w.id === wordId)?.priority,
+              }
+            : word
+        )
+      );
     }
   };
 
@@ -63,8 +98,8 @@ const WordTable: React.FC<WordTableProps> = ({ words }) => {
               </div>
             </TableHead>
             <TableHead className="border-r col-span-2 md:col-span-1 border-gray-300">
-              <div className="flex justify-center items-center h-full min-w-[40px] md:text-base text-xs whitespace-normal break-word">
-                Play Pron.
+              <div className="flex justify-center items-center h-full min-w-[40px] md:text-base text-xs whitespace-normal break-all">
+                Priority
               </div>
             </TableHead>
             <TableHead className="col-span-3 md:col-span-2">
@@ -98,22 +133,75 @@ const WordTable: React.FC<WordTableProps> = ({ words }) => {
                     {word.pronunciation}
                   </div>
                 </TableCell>
-                <TableCell className="p-1 border-r col-span-2 md:col-span-1 border-gray-300 relative">
-                  <div className="flex justify-center items-center h-full min-w-[20px] whitespace-normal break-all">
-                    <Button
-                      className="rounded-full bg-gray-700 hover:bg-gray-800 md:text-base text-xs px-2 py-0.3"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        new Audio(word.audiourl).play();
-                      }}
+                <TableCell className="p-1 md:px-8 text-xs col-span-2 md:col-span-1 md:text-base border-r border-gray-300 whitespace-normal break-all">
+                  <div className="flex justify-center items-center h-full">
+                    <Select
+                      value={word.priority || ""}
+                      onValueChange={(value: any) =>
+                        handlePriorityChange(word.id, value)
+                      }
                     >
-                      <FaCirclePlay className="mr-1 text-xs md:text-lg md:mr-2" />
-                      <div className="md:text-base text-xs">Play</div>
-                    </Button>
+                      <SelectTrigger className="w-[80px] sm:w-[100px] md:w-[120px] lg:w-[140px] xl:w-[160px] p-1 md:p-4">
+                        <SelectValue>
+                          <div className="flex items-center">
+                            {/* Display stars in a row based on the selected priority */}
+                            {word.priority === "star3" && (
+                              <>
+                                <FaStar className="text-yellow-500 text-xs sm:text-sm md:text-base" />
+                                <FaStar className="text-yellow-500 text-xs sm:text-sm md:text-base" />
+                                <FaStar className="text-yellow-500 text-xs sm:text-sm md:text-base" />
+                              </>
+                            )}
+                            {word.priority === "star2" && (
+                              <>
+                                <FaStar className="text-yellow-500 text-xs sm:text-sm md:text-base" />
+                                <FaStar className="text-yellow-500 text-xs sm:text-sm md:text-base" />
+                              </>
+                            )}
+                            {word.priority === "star1" && (
+                              <>
+                                <FaStar className="text-yellow-500 text-xs sm:text-sm md:text-base" />
+                              </>
+                            )}
+                            {word.priority === "remove" && (
+                              <span className="text-red-500 text-xs sm:text-sm md:text-base">
+                                Delete
+                              </span>
+                            )}
+                          </div>
+                        </SelectValue>
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem value="star3">
+                            <div className="flex items-center">
+                              <FaStar className="text-yellow-500 mr-1 text-xs sm:text-sm md:text-base" />
+                              <FaStar className="text-yellow-500 mr-1 text-xs sm:text-sm md:text-base" />
+                              <FaStar className="text-yellow-500 mr-1 text-xs sm:text-sm md:text-base" />
+                            </div>
+                          </SelectItem>
+                          <SelectItem value="star2">
+                            <div className="flex items-center">
+                              <FaStar className="text-yellow-500 mr-1 text-xs sm:text-sm md:text-base" />
+                              <FaStar className="text-yellow-500 mr-1 text-xs sm:text-sm md:text-base" />
+                            </div>
+                          </SelectItem>
+                          <SelectItem value="star1">
+                            <div className="flex items-center">
+                              <FaStar className="text-yellow-500 mr-1 text-xs sm:text-sm md:text-base" />
+                            </div>
+                          </SelectItem>
+                          <SelectItem value="remove">
+                            <span className="text-red-500 text-xs sm:text-sm md:text-base">
+                              Delete
+                            </span>
+                          </SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
                   </div>
-                  {/* Icon visible when the row is hovered */}
-                  <FaChevronDown className="absolute left-1 md:left-6 bottom-0 transform -translate-y-1/2 opacity-0 group-hover:opacity-30 transition-opacity" />
                 </TableCell>
+
                 <TableCell className="py-1 col-span-3 md:col-span-2 whitespace-normal break-word">
                   <ul className="list-disc list-inside sm:pl-5 md:text-lg text-xs">
                     {word.keymeanings.map((km, index) => (
@@ -135,7 +223,7 @@ const WordTable: React.FC<WordTableProps> = ({ words }) => {
                       className="grid grid-cols-1 bg-gray-50"
                     >
                       <TableCell
-                        colSpan={4}
+                        colSpan={5}
                         className="py-3 px-3 flex justify-center items-center whitespace-normal break-all"
                       >
                         <WordDetail
