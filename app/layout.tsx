7e197957fe63360e14f27b2988cc3f8c00f61548
Ykeisuke <g1d4h@students.cs.ubc.ca>
2024-08-18 02:04:21 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"
+          content="width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, interactive-widget=resizes-content"
         />
       </head>
       <body className={`${inter.className} h-[100dvh] overflow-hidden`}> 
