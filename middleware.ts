c9fc3da458425d1c13d190c49896be0c2a82ef50
Ykeisuke <g1d4h@students.cs.ubc.ca>
2024-08-28 22:43:36 -0700

    fix

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,6 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
   matcher: [
-    '/((?!api).*)',
-    '/((?!_next/static).*)',
-    '/((?!_next/image).*)',
-    '/((?!.*\\.(png|ico|jpg|jpeg|svg|webp|gif)$).*)'
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(png|jpg|jpeg|svg|webp|gif|ico)).*)',
   ],
 };
