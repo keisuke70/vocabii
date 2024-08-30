55842e186c928267eb6d776b0fe78c74cc90d5b3
Ykeisuke <g1d4h@students.cs.ubc.ca>
2024-08-30 02:13:32 -0700

    for one time payment

--- app/dashboard/setting/page.tsx
+++ app/dashboard/setting/page.tsx
@@ -1,13 +1,38 @@
-// pages/dashboard/setting.tsx or pages/dashboard/setting.js
-import React from 'react';
+"use client";
 
-const SettingsPage = () => {
+import CheckoutPage from "@/app/ui/standalone/checkoutPage";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
+  throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined");
+}
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+export default function Home() {
+  const amount = 700;
+
   return (
-    <div>
-      <h1>Settings</h1>
-      {/* Your settings page content */}
-    </div>
-  );
-};
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
+      <div className="mb-10">
+        <h1 className="text-4xl font-extrabold mb-2">Vocabii</h1>
+        <h2 className="text-2xl">
+          has requested
+          <span className="font-bold"> ¥{amount}</span>
+          <span className="text-lg"> /per month</span>
+        </h2>
+      </div>
 
-export default SettingsPage;
+      <Elements
+        stripe={stripePromise}
+        options={{
+          mode: "payment",
+          amount: amount,
+          currency: "jpy",
+        }}
+      >
+        <CheckoutPage amount={amount} />
+      </Elements>
+    </main>
+  );
+}
